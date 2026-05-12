1da3d48edc994bc23ec30e9399e38bd6c664f855
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-05-12 11:34:35 +0530

    Delete terraform-aks-cluster-and-gke-cluster-public directory

--- terraform-aks-cluster-and-gke-cluster-public/main/output.tf
+++
@@ -1,5 +0,0 @@
-output "azure_managed_identity_client_id" {
-  description = "Azure Managed Identity Client ID"
-  value       = module.aks_cluster_and_standard_gke_cluster 
-  sensitive   = true
-}
