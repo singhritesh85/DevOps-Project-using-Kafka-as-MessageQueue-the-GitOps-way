f3d40fa73c46893efdbaf0963f7d8382de10cb9d
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-05-14 20:36:01 +0530

    Delete module-1/terraform-aks-cluster-and-gke-cluster-public directory

--- module-1/terraform-aks-cluster-and-gke-cluster-public/main/output.tf
+++
@@ -1,5 +0,0 @@
-output "azure_managed_identity_client_id_and_azure_dns_zone_nameservers" {
-  description = "Azure Managed Identity Client ID, Azure DNS Zone and Nameservers"
-  value       = module.aks_cluster_and_standard_gke_cluster 
-  sensitive   = true
-}
